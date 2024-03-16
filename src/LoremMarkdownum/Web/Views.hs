c0b65a212e1ca64a32df6dd9100e8d619b30b11f
Jasper Van der Jeugt <m@jaspervdj.be>
2024-03-16 08:05:17 +0100

    remove google analytics

--- src/LoremMarkdownum/Web/Views.hs
+++ src/LoremMarkdownum/Web/Views.hs
@@ -119,15 +119,6 @@ index pc mc markdown = H.docTypeHtml $ do
             "    padding-left: 12px;"
             "}"
 
-        -- Google analytics
-        H.script ! A.type_ "text/javascript" $
-            "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){    \n\
-            \(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),  \n\
-            \m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) \n\
-            \})(window,document,'script','//www.google-analytics.com/analytics.js','ga');    \n\
-            \ga('create', 'UA-11993001-1', 'jaspervdj.be');                                  \n\
-            \ga('send', 'pageview');"
-
         H.script ! A.type_ "text/javascript" ! A.src "jquery-1.10.2.min.js" $ ""
         H.script ! A.type_ "text/javascript" ! A.src "lorem-markdownum.js"  $ ""
 
